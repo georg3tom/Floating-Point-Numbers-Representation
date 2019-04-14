6c5e045cdf4867b5288683b0f05c08f4038f7c36
George Tom <georg3tom@gmail.com>
2019-04-14 22:10:37 +0530

    commit yeah

--- app.py
+++ app.py
@@ -27,7 +27,21 @@ def manual():
 
 @app.route("/quizzes")
 def quizzes():
-    return render_template("quizzes.html")
+    submit = request.args.get('subm')
+    if int(submit) == 1 and request.args.get('q1') != None and request.args.get('q2') != None \
+    and request.args.get('q3') != None and request.args.get('q4') != None:
+        a1=a2=a3=a4=a5=0;
+        if(int(request.args.get('q1'))==None):
+            a1=1;
+        elif(int(request.args.get('q2'))==1):
+            a2=1;
+        elif(int(request.args.get('q3'))==1):
+            a3=1;
+        elif(int(request.args.get('q4'))==1):
+            a4=1;
+        return str(a1)+str(a2)+str(a3)+str(a4);
+    else:
+        return render_template("quizzes.html")
 
 @app.route("/procedure")
 def procedure():
